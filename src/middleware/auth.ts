543187998d92fb18edd670d2f6826707fbdd272a
Natan Santana <natan.santana.dev@gmail.com>
2021-09-01 13:33:55 -0300

    #60 fix eslint errors

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,30 +1,32 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 
 export default class AuthService {
+  decodeToken = async (token: string) => {
+    const data = await jwt.verify(token, 'secret');
+    return data;
+  };
 
-    decodeToken = async (token: string) => {
-        var data = await jwt.verify(token, 'secret');
-        return data;
-    }
+  authorize = (req: Request, res: Response, next: () => void) => {
+    const token =
+      req.body.token ||
+      req.query.token ||
+      req.headers.authorization?.split(' ')[1];
 
-    authorize = (req: Request, res: Response, next: () => void) => {
-        var token = req.body.token || req.query.token || req.headers.authorization?.split(' ')[1];
-
-        if (!token) {
-            res.status(401).json({
-                message: 'Acesso Restrito'
-            });
+    if (!token) {
+      res.status(401).json({
+        message: 'Acesso Restrito',
+      });
+    } else {
+      jwt.verify(token, 'secret', (error: any) => {
+        if (error) {
+          res.status(401).json({
+            message: 'Token Inválido',
+          });
         } else {
-            jwt.verify(token, 'secret', function (error: any, decoded: any) {
-                if (error) {
-                    res.status(401).json({
-                        message: 'Token Inválido'
-                    });
-                } else {
-                    next();
-                }
-            });
+          next();
         }
+      });
     }
+  };
 }
