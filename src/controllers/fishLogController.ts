f9a57116ba57ef2c4225bbbafa304cc93062c81a
Gabriel Freitas <gabriel.fbalbino@gmail.com>
2021-09-14 20:35:39 -0300

    #117 Change model to equal class diagram

--- src/controllers/fishLogController.ts
+++ src/controllers/fishLogController.ts
@@ -7,7 +7,7 @@ const auth = new AuthService();
 export default class FishController {
   createFishLog = async (req: Request, res: Response) => {
     try {
-      if (!(req.body.fishType || req.body.specie || req.body.photo)) {
+      if (!(req.body.largeGroup || req.body.specie || req.body.photo)) {
         return res.status(400).json({
           message:
             'Registro não foi criado, é necessário o tipo, a espécie ou a foto para a criação de um registro.',
--- src/models/fishLog.ts
+++ src/models/fishLog.ts
@@ -2,18 +2,19 @@ import { Schema, model, Document } from 'mongoose';
 
 interface IFishLog extends Document {
   userId: number;
-  fishType: string;
+  largeGroup: string;
   specie: string;
   coordenates: [number, number][];
   photo: string;
   lenght: number;
+  weight: number;
   reviewed: boolean;
   reviewedBy: number;
   createdAt: Date;
   updatedAt: Date;
-  deletedBy: number;
   updatedBy: number;
   deletedAt: Date;
+  deletedBy: number;
 }
 
 const fishLogSchema = new Schema<IFishLog>(
@@ -22,7 +23,7 @@ const fishLogSchema = new Schema<IFishLog>(
       type: Schema.Types.ObjectId,
       required: true,
     },
-    fishType: {
+    largeGroup: {
       type: String,
       enum: ['escama', 'couro', 'arraia', 'outros'],
       required: false,
@@ -43,6 +44,10 @@ const fishLogSchema = new Schema<IFishLog>(
       type: Number,
       required: false,
     },
+    weight: {
+      type: Number,
+      required: false,
+    },
     reviewed: {
       type: Boolean,
       default: false,
@@ -51,15 +56,15 @@ const fishLogSchema = new Schema<IFishLog>(
       type: Schema.Types.ObjectId,
       required: false,
     },
+    updatedBy: {
+      type: [Schema.Types.ObjectId],
+      required: false,
+    },
     deletedAt: {
       type: Date,
     },
     deletedBy: {
       type: Schema.Types.ObjectId,
-      required: false,
-    },
-    updatedBy: {
-      type: [Schema.Types.ObjectId],
       required: false,
     },
   },
