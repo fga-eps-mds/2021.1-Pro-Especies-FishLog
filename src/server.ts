90bd0d31ff7a8ad65d1f33ea1720a0032b003e2d
Pedro Rodrigues <peddroprp@gmail.com>
2021-10-06 02:13:06 -0300

    Add configs to deploy

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(4001, () => {
+const serverPort = process.env.PORT || 4001
+
+app.listen(serverPort, () => {
   console.log(`server running on port 4001`);
 });
