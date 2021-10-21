8cffaa7b0b7cf83c508d701f4df67d0406000757
Gabriel Freitas <gabriel.fbalbino@gmail.com>
2021-10-21 14:32:24 -0300

    #62 Att updateFishLog

--- src/controllers/fishLogController.ts
+++ src/controllers/fishLogController.ts
@@ -107,10 +107,18 @@ export default class FishController {
         (!fishLog.reviewed && String(fishLog?.userId) === data.id)
       ) {
         try {
-          if (!(req.body.largeGroup || req.body.species || req.body.photo)) {
+          if (
+            !(
+              req.body.largeGroup ||
+              req.body.group ||
+              req.body.name ||
+              req.body.species ||
+              req.body.photo
+            )
+          ) {
             return res.status(400).json({
               message:
-                'Registro não pode ser atualizado! É necessário o grande grupo, a espécie ou a foto para a atualização de um registro.',
+                'Registro não pode ser atualizado! É necessário pelo menos o grande grupo,grupo,nome,espécie ou a foto para a atualização de um registro.',
             });
           }
           await fishLog.updateOne(newFishLog);
